c68fde148b15def26ffe28f76bf18f3b9fbf508f
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2023-10-23 10:06:36 +0200

    инлайн клава

--- Bot.py
+++ Bot.py
@@ -1,7 +1,8 @@
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
 from telegram.ext import CallbackContext
 from telegram.ext import Filters
 from telegram.ext import Dispatcher
@@ -21,11 +22,15 @@ def main():
 
     start_handler = CommandHandler(('start', 'help'), start)
     keyboard_handler = CommandHandler('keyboard', keyboard)
+    inline_keyboard_handler = CommandHandler('inline_keyboard', inline_keyboard)
     echo_handler = MessageHandler(Filters.text, do_echo)
+    callback_handler = CallbackQueryHandler(keyboard_react)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(keyboard_handler)
+    dispatcher.add_handler(inline_keyboard_handler)
     dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(callback_handler)
 
     updater.start_polling()
     logger.info(updater.bot.getMe())
@@ -40,7 +45,7 @@ def do_echo(update: Update, context: CallbackContext):
 
     logger.info(f'{username=}({user_id=}) воззвал к Богу. Снизойти ли до его {text}?..')
     answer = f'Ты, {first_name}, взывающий(ая) к Богу, изрек(ла) {text}'
-    update.message.reply_text(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove)
 
 
 def start(update: Update, context: CallbackContext):
@@ -49,19 +54,23 @@ def start(update: Update, context: CallbackContext):
 
     logger.info(f'{username=} решил, что достоен диалога с Богом.')
     text = [
-        f'{first_name}, смотрю, ты решил(a), что достоен диалога с Богом..',
+        f'Приветствую тебя, {first_name}, смотрю, ты решил(a), что достоен диалога с Богом..',
         'В моей келье ты можешь:',
-        ''
+        '/start - посмотреть команды и поприветствовать твоего сегодняшнего наставника.',
+        '/keyboard - вызвать клавиатуру, коли ты так ленив.',
+        '/inline_keyboard - вызвать прикрепленную к сообщению клавиатуру, коли ты совсем ленивю'
         ]
     text = '\n'.join(text)
     update.message.reply_text(text)
 
 
 def keyboard(update: Update, context: CallbackContext):
+    username = update.message.from_user.username
+    logger.info(f'{username=} ленится и вызвал инлайн клаву.')
     buttons = [
-        ['Прочитать молитву', 'Прочитать молитву'],
-        ['Прочитать молитву','Прочитать молитву'],
-        ['Прочитать молитву']
+        ['Прочитать молитву 1', 'Прочитать молитву 2'],
+        ['Прочитать молитву 3','Прочитать молитву 4'],
+        ['Прочитать молитву 5']
     ]
     keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
 
@@ -71,7 +80,47 @@ def keyboard(update: Update, context: CallbackContext):
         text,
         reply_markup=keyboard
     )
+    logger.info('живем, не ломаемся')
+
+def inline_keyboard(update: Update, context: CallbackContext):
+    username = update.message.from_user.username
+    logger.info(f'{username=} ленится и вызвал инлайн клаву.')
+    buttons = [
+        ['Прочитать молитву 1', 'Прочитать молитву 2'],
+        ['Прочитать молитву 3', 'Прочитать молитву 4'],
+        ['Прочитать молитву 5']
+    ]
+    keyboard_buttons = [[InlineKeyboardButton(text=text, callback_data=text) for text in row] for row in buttons]
+    keyboard = InlineKeyboardMarkup(keyboard_buttons)
+    text = 'Ну раз ты так ленив, выбери свою молитву на сегодня.'
+    update.message.reply_text(
+        text,
+        reply_markup=keyboard
+    )
+    logger.info('живем, не ломаемся')
 
 
+def keyboard_react(update: Update, context: CallbackContext):
+    query = update.callback_query
+    username = update.effective_user.username
+    logger.info(f'{username=} решил поиграться кнопочками.')
+    buttons = [
+        ['Прочитать молитву 1', 'Прочитать молитву 2'],
+        ['Прочитать молитву 3', 'Прочитать молитву 4'],
+        ['Прочитать молитву 5']
+    ]
+    for row in buttons:
+        if query.data in row:
+            row.pop(row.index(query.data))
+    keyboard_buttons = [[InlineKeyboardButton(text=text, callback_data=text) for text in row] for row in buttons]
+    keyboard = InlineKeyboardMarkup(keyboard_buttons)
+    text = 'Ну раз ты так ленив, выбери свою молитву на сегодня.'
+    query.edit_message_text(
+        text,
+        reply_markup=keyboard
+    )
+    logger.info('живем, не ломаемся')
 
-main()
+
+if __name__ == '__main__':
+    main()
