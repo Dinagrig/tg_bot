e7b08f51d5882658571c247385c7e2e747e24d73
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2023-12-01 11:59:06 +0200

    начала создавать персонажа

---
+++ char.py
@@ -0,0 +1,8 @@
+class Character:
+
+    def __init__(self, name, surname, birthday, teeth, hair_color):
+        self.name = name
+        self.surname = surname
+        self.birthday = birthday
+        self.teeth = teeth
+        self.hair_color = hair_color
