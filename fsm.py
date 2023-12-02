4ecc1ea38c8d84bca9ecf82f694a3b2f479fe56c
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2023-12-02 20:46:58 +0300

    Два новых состояния и возможность повторной реги

--- fsm.py
+++ fsm.py
@@ -7,12 +7,46 @@ from telegram.ext import CallbackContext
 from telegram.ext import ConversationHandler
 from telegram.ext import Filters
 
-from db import write_to_db
+from db import write_to_db, find_user_by_id
 import logging
 
 logger = logging.getLogger(__name__)
 
-WAIT_NAME, WAIT_SURNAME, WAIT_BIRTHDAY = range(3)
+WAIT_OK, WAIT_NAME, WAIT_SURNAME, WAIT_BIRTHDAY, WAIT_TEETH, WAIT_HAIRCLR = range(6)
+
+
+def check_register(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    logger.info(f'{username=} {user_id=} хочет проверить регистрацию.')
+    user = find_user_by_id(user_id)
+    if not user:
+        return ask_name(update, context)
+    answer = [f'Приветствую.',
+              f'Ты уже есть в списке собеседников Божьих со следующими данными:',
+              f'Имя твое: {user[1]},',
+              f'Фамилия твоя: {user[2]}',
+              f'День рождения твоего: {user[3]}',
+              f'Пломбы в устах твоих (ложь): {user[4]}',
+              f'Цвет воолос твоих: {user[5]}']
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
+
+    buttons = [InlineKeyboardButton(text='Да', callback_data='Да'),
+               InlineKeyboardButton(text='Нет', callback_data='Нет')]
+    keyboard = InlineKeyboardMarkup.from_row(buttons)
+    update.message.reply_text(text='Ты хочешь повторно стать собеседников Божьим?', reply_markup=keyboard)
+    return WAIT_OK
+
+
+def get_yes_no(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    logger.info(f'{username=} {user_id=} ответил, хочет ли регаться снова.')
+    query = update.callback_query
+    if query.data == 'Да':
+        return ask_name(update, context)
+    return ConversationHandler.END
 
 
 def ask_name(update: Update, context: CallbackContext):
@@ -22,7 +56,7 @@ def ask_name(update: Update, context: CallbackContext):
     logger.info(f'{username}{user_id} хочет сообщить имя свое ("ask_name").')
     answer = [
         f'Приветствую!\n'
-        f'Скажи мне имя свое.\n'
+        f'Назови мне имя свое.\n'
 
     ]
     answer = '\n'.join(answer)
@@ -86,26 +120,80 @@ def ask_birthday(update: Update, context: CallbackContext):
     return WAIT_BIRTHDAY
 
 def get_birthday(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    textb = update.message.text
+    context.user_data['birthday'] = textb
+    logger.info(f'{username}{user_id} запросил день рождения своего ("get_birthday").')
+    answer = [
+        f'Твой день рождения - {textb}\n'
+
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
+    return ask_teeth(update, context)
+
+def ask_teeth(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    logger.info(f'{username}{user_id} хочет доложить про пломбы)))))) ("ask_teeth").')
+    answer = [
+        f'Ладно....\n'
+        f'Назови мне количество пломб в устах твоих.\n'
+
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
+    return WAIT_TEETH
+
+def get_teeth(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    textt = update.message.text
+    context.user_data['teeth'] = textt
+    logger.info(f'{username}{user_id} спросил про зубы свои ("get_teeth").')
+    answer = [
+        f'Я, конечно знаю, что ты солгал, но зачту. Количество пломб в устах твоих - {textt}\n'
+
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
+    return ask_hairclr(update, context)
+
+def ask_hairclr(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    logger.info(f'{username}{user_id} хочет сказать цвет волос своих ("ask_hairclr").')
+    answer = [
+        f'Учту..\n'
+        f'Назови мне цвет волос своих.\n'
+
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
+    return WAIT_HAIRCLR
+
+
+def get_hairclr(update: Update, context: CallbackContext):
     return register(update, context)
 
 
 def register(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     username = update.message.from_user.username
-    textb = update.message.text
-    context.user_data['birthday'] = textb
+    texth = update.message.text
+    context.user_data['hair'] = texth
+    logger.info(f'{username}{user_id}: допрос завершен.')
+    answer = 'Благодарю.'
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
+
     userid = context.user_data['userid']
     name = context.user_data['name']
     surname = context.user_data['surname']
     birthday = context.user_data['birthday']
+    teeth = context.user_data['teeth']
+    hair = context.user_data['hair']
     write_to_db(userid, name, surname, birthday)
-    logger.info(f'{username}{user_id} ??"".')
-    answer = [
-        f'Благодарю!\n'
-
-    ]
-    answer = '\n'.join(answer)
-    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
 
@@ -114,7 +202,9 @@ register_handler = ConversationHandler(
     states={
         WAIT_NAME: [MessageHandler(Filters.text, get_name)],
         WAIT_SURNAME: [MessageHandler(Filters.text, get_surname)],
-        WAIT_BIRTHDAY: [MessageHandler(Filters.text, get_birthday)]
+        WAIT_BIRTHDAY: [MessageHandler(Filters.text, get_birthday)],
+        WAIT_TEETH: [MessageHandler(Filters.text, get_teeth)],
+        WAIT_HAIRCLR: [MessageHandler(Filters.text, get_hairclr)],
     },
     fallbacks=[]
 )
