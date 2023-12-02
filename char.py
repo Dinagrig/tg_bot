b03ab874ff2c0f564b8f3250cee6dcd8f4617b7c
Dinagrig <148317173+Dinagrig@users.noreply.github.com>
2023-12-02 19:48:22 +0200

    Delete char.py

--- char.py
+++
@@ -1,8 +0,0 @@
-class Character:
-
-    def __init__(self, name, surname, birthday, teeth, hair_color):
-        self.name = name
-        self.surname = surname
-        self.birthday = birthday
-        self.teeth = teeth
-        self.hair_color = hair_color
